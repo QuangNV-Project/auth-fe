8c6adb817c33e754ebc335caaf72cd48e142ef9b
Nguyen Vinh Quang <quangnv1911>
2026-03-05 10:41:43 +0700

    feat: update tenant code header

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,7 @@
 const ENV = {
   BACK_END_URL: import.meta.env.VITE_BACK_END_URL as string,
   GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID as string,
+  TENANT_CODE: import.meta.env.VITE_TENANT_CODE as string,
 }
 
 export { ENV }
